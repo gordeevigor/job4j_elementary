caea95f2e9e52fcc4ccc1d0b84c6ce9ff8a13a25
gordeevigor <mantraspirit@gmail.com>
2024-07-17 20:32:49 +0400

    1.Actual Examples class for completed tasks

--- src/main/java/ru/job4j/Examples.java
+++ src/main/java/ru/job4j/Examples.java
@@ -1,11 +1,12 @@
 package ru.job4j;
 
 public class Examples {
+    public static boolean greatThen(int first, int second) {
+        return first > second;
+    }
+
     public static void main(String[] args) {
-        short s = 31000;
-        char c = (char) s;
-        long d = (long) c;
-        System.out.println("Вывод в консоль значения после преобразования: " + c);
-        System.out.println("Вывод в консоль значения после преобразования: " + d);
+        boolean result = greatThen(10, 2);
+        System.out.println(result);
     }
 }
