94fe392d3a240c8f24516a21bd17b60b207bb242
gordeevigor <mantraspirit@gmail.com>
2024-07-21 17:51:07 +0400

    1.1 PrimeNumber checker without else operator;

--- src/main/java/ru/job4j/loop/CheckPrimeNumber.java
+++ src/main/java/ru/job4j/loop/CheckPrimeNumber.java
@@ -2,13 +2,14 @@ package ru.job4j.loop;
 
 public class CheckPrimeNumber {
     public static boolean check(int number) {
-        boolean prime = false;
+        boolean prime = true;
+        if (number < 2) {
+            return false;
+        }
         for (int i = 2; i < number; i++) {
             if ((number % i) == 0) {
                 prime = false;
                 break;
-            } else {
-                prime = true;
             }
         }
         return prime;
