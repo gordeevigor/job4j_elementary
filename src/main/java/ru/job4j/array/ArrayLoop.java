d8eddd9d555e38b2ea9dfc5eaff74237b6a935e9
gordeevigor <mantraspirit@gmail.com>
2024-07-24 20:40:00 +0400

    1. ArrayLoop class with for - each operator.

--- src/main/java/ru/job4j/array/ArrayLoop.java
+++ src/main/java/ru/job4j/array/ArrayLoop.java
@@ -6,8 +6,8 @@ public class ArrayLoop {
         for (int i = 0; i < y.length; i++) {
             y[i] = i * 2 + 3;
         }
-        for (int i = 0; i < y.length; i++) {
-            System.out.println(y[i]);
+        for (int j : y) {
+            System.out.println(j);
         }
     }
 }
