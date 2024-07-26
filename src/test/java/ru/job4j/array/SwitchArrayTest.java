44ddb50adbabda85259cb42dfe12b53116dac047
gordeevigor <mantraspirit@gmail.com>
2024-07-26 21:23:51 +0400

    1. Add swap method in SwitchArray class, changing unit tests.

--- src/test/java/ru/job4j/array/SwitchArrayTest.java
+++ src/test/java/ru/job4j/array/SwitchArrayTest.java
@@ -36,4 +36,34 @@ class SwitchArrayTest {
         int[] expected = {1};
         assertThat(result).containsExactly(expected);
     }
+
+    @Test
+    void whenSwap0to3() {
+        int[] input = {1, 2, 3, 4};
+        int source = 0;
+        int destination = input.length - 1;
+        int[] result = SwitchArray.swap(input, source, destination);
+        int[] expected = {4, 2, 3, 1};
+        assertThat(result).containsExactly(expected);
+    }
+
+    @Test
+    void whenSwap3to7() {
+        int[] input = {9, 8, 7, 6, 5, 4, 3, 2, 1, 0};
+        int source = 3;
+        int destination = 7;
+        int[] result = SwitchArray.swap(input, source, destination);
+        int[] expected = {9, 8, 7, 2, 5, 4, 3, 6, 1, 0};
+        assertThat(result).containsExactly(expected);
+    }
+
+    @Test
+    void whenSwap4to5() {
+        int[] input = {11, 2, 0, 3, 26, 42, 233, 12};
+        int source = 4;
+        int destination = 5;
+        int[] result = SwitchArray.swap(input, source, destination);
+        int[] expected = {11, 2, 0, 3, 42, 26, 233, 12};
+        assertThat(result).containsExactly(expected);
+    }
 }
