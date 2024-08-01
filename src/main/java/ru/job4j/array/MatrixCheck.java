10e4f6c3d3a8ba40b7f91edfe6a25404218fb886
gordeevigor <mantraspirit@gmail.com>
2024-08-01 21:55:12 +0400

    1.1 Character 'X' check in isWin method.

--- src/main/java/ru/job4j/array/MatrixCheck.java
+++ src/main/java/ru/job4j/array/MatrixCheck.java
@@ -34,8 +34,8 @@ public class MatrixCheck {
     public static boolean isWin(char[][] board) {
         boolean result = false;
         for (int i = 0; i < board.length; i++) {
-            if (monoVertical(board, i) || monoHorizontal(board, i)) {
-                result = true;
+            if (board[i][i] == 'X') {
+                result = monoVertical(board, i) || monoHorizontal(board, i);
                 break;
             }
         }
