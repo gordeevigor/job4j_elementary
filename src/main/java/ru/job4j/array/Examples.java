211887df4217f5d9a7680f3caed2aefd8c92095e
gordeevigor <mantraspirit@gmail.com>
2024-08-02 19:56:29 +0400

    1. Add Examples class for experiments

---
+++ src/main/java/ru/job4j/array/Examples.java
@@ -0,0 +1,18 @@
+package ru.job4j.array;
+
+public class Examples {
+    public static int[] compress(int[] array) {
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] == 0) {
+                for (int j = i + 1; j < array.length; j++) {
+                    if (array[j] != 0) {
+                        array[i] = array[j];
+                        array[j] = 0;
+                        break;
+                    }
+                }
+            }
+        }
+        return array;
+    }
+}
