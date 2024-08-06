bc862cc66c3f72c249f085451b3ff61631b53c78
gordeevigor <mantraspirit@gmail.com>
2024-08-06 12:26:19 +0400

    1.1 Getting rid of variables 1.2 Using for-each

--- src/main/java/ru/job4j/array/Machine.java
+++ src/main/java/ru/job4j/array/Machine.java
@@ -7,11 +7,10 @@ public class Machine {
         int[] coins = {10, 5, 2, 1};
         int[] result = new int[100];
         int size = 0;
-        int moneyChange = money - price;
-        for (int i = 0; i < coins.length; i++) {
-            while (moneyChange >= coins[i]) {
-                moneyChange -= coins[i];
-                result[size] = coins[i];
+        for (int coin : coins) {
+            while (money - price >= coin) {
+                money -= coin;
+                result[size] = coin;
                 size++;
             }
         }
