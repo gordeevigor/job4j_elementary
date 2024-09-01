7486ad46820a8bdf76f22aca14739f594117a63a
gordeevigor <mantraspirit@gmail.com>
2024-09-01 13:38:12 +0400

    1. Loop tasks 66 - 69 resolved

--- src/main/java/ru/job4j/practice/loop/Task70.java
+++ src/main/java/ru/job4j/practice/loop/Task70.java
@@ -1,4 +1,30 @@
 package ru.job4j.practice.loop;
 
 public class Task70 {
+    public static void loop(int a, int b, int r, int[] coords) {
+        int onCircle = 0;
+        int insideCircle = 0;
+
+        // Квадрат радиуса для сравнения
+        int rSquared = r * r;
+
+        // Перебираем массив координат с шагом 2
+        for (int i = 0; i < coords.length; i += 2) {
+            int x = coords[i];
+            int y = coords[i + 1];
+
+            // Вычисляем квадрат расстояния от центра окружности до точки (x, y)
+            int distanceSquared = (x - a) * (x - a) + (y - b) * (y - b);
+
+            // Определяем положение точки относительно окружности
+            if (distanceSquared == rSquared) {
+                onCircle++;
+            } else if (distanceSquared < rSquared) {
+                insideCircle++;
+            }
+        }
+
+        // Выводим результат
+        System.out.println("На окружности: " + onCircle + ", внутри: " + insideCircle);
+    }
 }
