20c2ac9f6b9c5a8285fde8b88f99b82c1cec5690
gordeevigor <mantraspirit@gmail.com>
2024-09-09 12:14:59 +0400

    1. Loop tasks 116 - 117 resolved

--- src/main/java/ru/job4j/Main.java
+++ src/main/java/ru/job4j/Main.java
@@ -5,6 +5,6 @@ import ru.job4j.practice.loop.*;
 public class Main {
     public static void main(String[] args) {
         int[] nums = {1, 2, 3, 4, 2, 6};
-        Task115.loop(nums);
+        Task117.loop(840);
     }
 }
--- src/main/java/ru/job4j/practice/loop/Task117.java
+++ src/main/java/ru/job4j/practice/loop/Task117.java
@@ -1,14 +1,16 @@
 package ru.job4j.practice.loop;
 
-public class Task116 {
+public class Task117 {
     public static void loop(int num) {
         int count = 0;
         int sum = 0;
         int temp = num;
         do {
             int digit = temp % 10;
+            if (digit % 2 == 0) {
             sum += digit;
             count++;
+            }
             temp /= 10;
         } while (temp > 0);
         System.out.println("Количество: " + count + ", сумма: " + sum);
