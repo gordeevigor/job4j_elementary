ac4393122a848678e0cd18a7a4d666638e012857
gordeevigor <mantraspirit@gmail.com>
2024-09-15 12:47:56 +0400

    1. Loop tasks 142 resolved

---
+++ src/main/java/ru/job4j/practice/loop/Task142.java
@@ -0,0 +1,28 @@
+package ru.job4j.practice.loop;
+
+public class Task142 {
+    public static void loop(int num) {
+        if (num <= 0) {
+            System.out.println("Да");
+            return;
+        }
+
+        int[] fibonachi = new int[Math.max(num, 5)];
+        fibonachi[0] = 0;
+        fibonachi[1] = 1;
+        fibonachi[2] = 1;
+        fibonachi[3] = 2;
+        fibonachi[4] = 3;
+        for (int i = 5; i < fibonachi.length; i++) {
+            fibonachi[i] = fibonachi[i - 1] + fibonachi[i - 2];
+        }
+        boolean found = false;
+        for (int f : fibonachi) {
+            if (f == num) {
+                found = true;
+                break;
+            }
+        }
+        System.out.println(found ? "Да" : "Нет");
+    }
+}
