33c253ba1938f5ca3b2586f35acd8518660b0050
gordeevigor <mantraspirit@gmail.com>
2024-09-18 14:30:26 +0400

    1. Loop task 151 resolved

--- src/main/java/ru/job4j/Main.java
+++ src/main/java/ru/job4j/Main.java
@@ -4,7 +4,6 @@ import ru.job4j.practice.loop.*;
 
 public class Main {
     public static void main(String[] args) {
-        int[] transfers = {-500, -100, 0, 300};
-        Task150.loop(-100, 500, transfers);
+        Task151.loop();
     }
 }
