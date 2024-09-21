135942f5514e2aa3feed13946318944a76d4c6a0
gordeevigor <mantraspirit@gmail.com>
2024-09-21 12:32:32 +0400

    1. Variables with long names rename

--- src/main/java/ru/job4j/practice/loop/Task153.java
+++ src/main/java/ru/job4j/practice/loop/Task153.java
@@ -3,10 +3,10 @@ package ru.job4j.practice.loop;
 public class Task153 {
     public static void loop() {
         int money = 100;
-        int bullPrice = 10;
-        int cowPrice = 5;
-        for (int i = 0; i <= money / bullPrice; i++) {
-            int cow = (money - bullPrice * i) / cowPrice;
+        int bullprice = 10;
+        int cowprice = 5;
+        for (int i = 0; i <= money / bullprice; i++) {
+            int cow = (money - bullprice * i) / cowprice;
             System.out.println("Быков: " + i + ", коров: " + cow);
         }
     }
