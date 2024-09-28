b3d7f0a6ca0c9e6b72dad2b5cd38614a27592afa
gordeevigor <mantraspirit@gmail.com>
2024-09-28 13:44:20 +0400

    1. Class Point and unit-test class PointTest OOP refactoring.

--- src/test/java/ru/job4j/condition/PointTest.java
+++ src/test/java/ru/job4j/condition/PointTest.java
@@ -5,25 +5,22 @@ import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.*;
 
 class PointTest {
+
     @Test
     void when00to20then2() {
-        int x1 = 0;
-        int y1 = 0;
-        int x2 = 2;
-        int y2 = 0;
+        Point a = new Point(0, 0);
+        Point b = new Point(0, 2);
         double expected = 2;
-        double output = Point.distance(x1, y1, x2, y2);
+        double output = a.distance(b);
         assertThat(output).isEqualTo(expected, withPrecision(0.01));
     }
 
     @Test
     void whenMinus20to3Minus1then5dot099() {
-        int x1 = -2;
-        int y1 = 0;
-        int x2 = 3;
-        int y2 = -1;
+        Point a = new Point(-2, 0);
+        Point b = new Point(3, -1);
         double expected = 5.099;
-        double output = Point.distance(x1, y1, x2, y2);
+        double output = a.distance(b);
         assertThat(output).isEqualTo(expected, withPrecision(0.01));
     }
 }
