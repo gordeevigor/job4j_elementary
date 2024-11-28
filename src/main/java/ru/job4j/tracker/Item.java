25129a7094f43a7a4baaa84aa86c78ecdb633d9d
gordeevigor <mantraspirit@gmail.com>
2024-11-28 22:46:08 +0400

    1. Create Item class for Tracker project

---
+++ src/main/java/ru/job4j/tracker/Item.java
@@ -0,0 +1,41 @@
+package ru.job4j.tracker;
+
+import java.time.LocalDateTime;
+
+public class Item {
+    private int id;
+    private String name;
+    private LocalDateTime created = LocalDateTime.now();
+
+    public Item() {
+    }
+
+    public Item(String name) {
+        this.name = name;
+    }
+
+    public Item(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public LocalDateTime getCreated() {
+        return created;
+    }
+}
