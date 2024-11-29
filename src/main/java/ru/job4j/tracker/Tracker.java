a03b4f8826fb45fd5936b2142b096883432219e0
gordeevigor <mantraspirit@gmail.com>
2024-11-29 20:35:32 +0400

    1. Create search methods for Tracker project

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -1,11 +1,13 @@
 package ru.job4j.tracker;
 
+import java.util.Arrays;
+
 public class Tracker {
     private final Item[] items = new Item[100];
     private int ids = 1;
     private int size = 0;
 
-    public Item addItem(Item item) {
+    public Item add(Item item) {
         item.setId(ids++);
         items[size++] = item;
         return item;
@@ -21,5 +23,22 @@ public class Tracker {
             }
         }
         return result;
+    }
+
+    public Item[] findAll() {
+        return Arrays.copyOf(items, size);
+    }
+
+    public Item[] findByName(String key) {
+        Item[] result = items;
+        int count = 0;
+        for (int i = 0; i < size; i++) {
+            Item item = items[i];
+            if (key.equals(item.getName())) {
+                result[count] = item;
+                count++;
+            }
+        }
+        return Arrays.copyOf(result, count);
     }
 }
