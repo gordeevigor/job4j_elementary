120dd1b172166034bc30730dc4563504e3a1e3f6
gordeevigor <mantraspirit@gmail.com>
2024-12-01 13:04:10 +0400

    1. Create delete method for Tracker project

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -55,4 +55,14 @@ public class Tracker {
         }
         return result;
     }
+
+    public void delete(int id) {
+        int index = indexOf(id);
+        boolean result = index != -1;
+        if (result) {
+            System.arraycopy(items, index + 1, items, index, size - index - 1);
+            items[size - 1] = null;
+            size--;
+        }
+    }
 }
