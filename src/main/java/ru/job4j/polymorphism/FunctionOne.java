614c1b1d3b0bf5c145d683c1bccd73bec56572aa
gordeevigor <mantraspirit@gmail.com>
2024-12-11 23:09:09 +0400

    1. Interface for tracker

--- src/main/java/ru/job4j/polymorphism/FunctionOne.java
+++ src/main/java/ru/job4j/polymorphism/FunctionOne.java
@@ -8,4 +8,20 @@ public interface FunctionOne {
     default void functionMessage() {
         System.out.println("Сообщение из FunctionOne");
     }
+
+    default int getDoubleSum(int[] numbers) {
+        return getSum(numbers) * 2;
+    }
+
+    default double getAverage(int[] numbers) {
+        return getSum(numbers) * 1.0 / numbers.length;
+    }
+
+    private int getSum(int[] numbers) {
+        int sum = 0;
+        for (int number : numbers) {
+            sum += number;
+        }
+        return sum;
+    }
 }
