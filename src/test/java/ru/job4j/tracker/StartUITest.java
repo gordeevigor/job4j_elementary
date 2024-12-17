b71c340114cb71882f3ff8219bbcf0c408705f12
gordeevigor <mantraspirit@gmail.com>
2024-12-17 22:55:54 +0400

    1. Unit-test on StartUI method createItem. Multiply method call

--- src/test/java/ru/job4j/tracker/StartUITest.java
+++ src/test/java/ru/job4j/tracker/StartUITest.java
@@ -7,12 +7,16 @@ import static org.assertj.core.api.Assertions.*;
 public class StartUITest {
     @Test
     void whenAddItem() {
-        String[] answers = {"Fix PC"};
+        String[] answers = {"Fix PC", "Fix iPhone"};
         Input input = new MockInput(answers);
         Tracker tracker = new Tracker();
         StartUI.createItem(input, tracker);
         Item created = tracker.findAll()[0];
         Item expected = new Item("Fix PC");
+        StartUI.createItem(input, tracker);
+        Item created1 = tracker.findAll()[1];
+        Item expected1 = new Item("Fix iPhone");
         assertThat(created.getName()).isEqualTo(expected.getName());
+        assertThat(created1.getName()).isEqualTo(expected1.getName());
     }
 }
